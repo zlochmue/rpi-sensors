5a855c0c69f217bcd0053d3a3cd7c4fa22b65c59
zlochmue <95509424+zlochmue@users.noreply.github.com>
2022-05-05 02:44:09 -0500

    second pass
    added column in db for outside temp measurement from another api to compare

---
+++ sql.py
@@ -0,0 +1,39 @@
+import os
+import mariadb as db
+import time
+from datetime import datetime
+import random
+from weath import get_current_temp
+
+start_time = time.time()
+start_now = datetime.now()
+while True:
+    # scans once every sec_between_scan seconds 
+    sec_between_scan = 30
+    time.sleep(sec_between_scan- time.time() % sec_between_scan)
+    print("Connecting to Database...")
+    start_now = datetime.now()
+    formatted_date = start_now.strftime('%Y-%m-%d %H:%M:%S')
+    username = os.environ.get("username")
+    password = os.environ.get("password")
+
+    conn = db.connect(user='zeek',
+                password= 'syzygy',
+                host= "localhost",
+                database= "temps")
+
+    cur = conn.cursor()
+s
+    print("Measuring temperature...")
+    sql= "INSERT INTO temps (entry, temp1, temp2, temp3) VALUES (?,?,?,?)"
+    rand1= random.randint(0,100) # change to temp sensor take
+    rand2= random.randint(0,100) # change to temp sensor take
+    curr = get_current_temp()
+    data= (formatted_date, rand1, rand2, curr)
+    print("Sending data...")
+    cur.execute(sql, data)
+
+    conn.commit()
+
+    cur.close()
+    conn.close()
---
+++ weath.py
@@ -0,0 +1,10 @@
+import requests
+import json
+
+def get_current_temp():
+	curr = 'https://api.openweathermap.org/data/2.5/weather?lat=41.87&lon=-87.62&appid=84105f1604d031a12cbcee0084df2326&units=imperial'
+	response = requests.get(curr)
+	y = response.json()
+	itera = json.dumps(y,indent=3)
+	y = json.loads(itera)
+	return float(y["main"]["temp"])
