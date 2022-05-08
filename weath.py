e8ccbb6012bcaeb050f9aec10c1026253386a50f
zlochmue <95509424+zlochmue@users.noreply.github.com>
2022-05-07 22:25:21 -0500

    Update weath.py

--- weath.py
+++ weath.py
@@ -1,6 +1,7 @@
 import requests
 import json
 
+# return the current temperature in Chicago from weather API
 def get_current_temp():
 	curr = 'https://api.openweathermap.org/data/2.5/weather?lat=41.87&lon=-87.62&appid=84105f1604d031a12cbcee0084df2326&units=imperial'
 	response = requests.get(curr)
